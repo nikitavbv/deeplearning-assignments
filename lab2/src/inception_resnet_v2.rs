1751359d3fd08acd538a3539fe93cf4d28f61895
Nikita Volobuev <nikitavbv@gmail.com>
2023-06-13 00:53:20 +0300

    feat(lab2): implement transition c

--- lab2/src/inception_resnet_v2.rs
+++ lab2/src/inception_resnet_v2.rs
@@ -8,6 +8,7 @@ use tch::{
         Conv2D, 
         BatchNorm, 
         ConvConfig, 
+        ConvConfigND,
         Path, 
         BatchNormConfig,
         SequentialT,
@@ -107,6 +108,23 @@ impl ConvLayer {
             }),
         }
     }
+
+    pub fn new_2d(path: &Path<'_>, input: i64, output: i64, kernel_size: (i64, i64), stride: (i64, i64), padding: (i64, i64)) -> Self {
+        Self {
+            conv: nn::conv(path, input, output, [kernel_size.0, kernel_size.1], ConvConfigND {
+                padding: [padding.0, padding.1],
+                bias: false,
+                stride: [stride.0, stride.1],
+                ..Default::default()
+            }),
+            batch_norm: nn::batch_norm2d(path, output, BatchNormConfig {
+                eps: 0.001,
+                momentum: 0.1,
+                affine: true,
+                ..Default::default()
+            })
+        }
+    }
 }
 
 impl ModuleT for ConvLayer {
@@ -198,9 +216,103 @@ impl ModuleT for InceptionBlockA {
     }
 }
 
-// TODO: implement this
 #[derive(Debug)]
 struct TransitionB {
     branch0: ConvLayer,
     branch1: SequentialT,
+}
+
+impl TransitionB {
+    pub fn new(path: &Path<'_>) -> Self {
+        Self {
+            branch0: ConvLayer::new(path, 320, 384, 3, 2, 0),
+            branch1: seq_t()
+                .add(ConvLayer::new(path, 320, 256, 1, 1, 0))
+                .add(ConvLayer::new(path, 256, 256, 3, 1, 1))
+                .add(ConvLayer::new(path, 256, 384, 3, 2, 0)),
+        }
+    }
+}
+
+impl ModuleT for TransitionB {
+    fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
+        let x0 = xs.apply_t(&self.branch0, train);
+        let x1 = xs.apply_t(&self.branch1, train);
+        let x2 = xs.max_pool2d([3, 3], [2, 2], [0, 0], [1, 1], false);
+        Tensor::cat(&[x0, x1, x2], 1)
+    }
+}
+
+#[derive(Debug)]
+struct InceptionBlockB {
+    scale: f32,
+
+    branch0: ConvLayer,
+    branch1: SequentialT,
+
+    conv1: Conv2D,
+}
+
+impl InceptionBlockB {
+    pub fn new(path: &Path<'_>, scale: f32) -> Self {
+        Self {
+            scale,
+
+            branch0: ConvLayer::new(path, 1088, 192, 1, 1, 0),
+            branch1: seq_t()
+                .add(ConvLayer::new(path, 1088, 128, 1, 1, 0))
+                .add(ConvLayer::new_2d(path, 128, 160, (1, 7), (1, 1), (0, 3)))
+                .add(ConvLayer::new_2d(path, 160, 192, (7, 1), (1, 1), (3, 0))),
+
+            conv1: nn::conv2d(path, 384, 1088, 1, ConvConfig {
+                stride: 1,
+                ..Default::default()
+            }),
+        }
+    }
+}
+
+impl ModuleT for InceptionBlockB {
+    fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
+        let x0 = xs.apply_t(&self.branch0, train);
+        let x1 = xs.apply_t(&self.branch1, train);
+
+        let out = Tensor::cat(&[x0, x1], 1).apply_t(&self.conv1, train);
+        let out = self.scale * out + xs;
+        out.relu()
+    }
+}
+
+#[derive(Debug)]
+struct TransitionC {
+    branch0: SequentialT,
+    branch1: SequentialT,
+    branch2: SequentialT,
+}
+
+impl TransitionC {
+    pub fn new(path: &Path<'_>) -> Self {
+        Self {
+            branch0: seq_t()
+                .add(ConvLayer::new(path, 1088, 256, 1, 1, 0))
+                .add(ConvLayer::new(path, 256, 384, 3, 2, 0)),
+            branch1: seq_t()
+                .add(ConvLayer::new(path, 1088, 256, 1, 1, 0))
+                .add(ConvLayer::new(path, 256, 258, 3, 2, 0)),
+            branch2: seq_t()
+                .add(ConvLayer::new(path, 1088, 256, 1, 1, 0))
+                .add(ConvLayer::new(path, 256, 288, 3, 1, 1))
+                .add(ConvLayer::new(path, 288, 320, 3, 2, 0)),
+        }
+    }
+}
+
+impl ModuleT for TransitionC {
+    fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
+        let x0 = xs.apply_t(&self.branch0, train);
+        let x1 = xs.apply_t(&self.branch1, train);
+        let x2 = xs.apply_t(&self.branch2, train);
+        let x3 = xs.max_pool2d([3, 3], [2, 2], [0, 0], [1, 1], false);
+        Tensor::cat(&[x0, x1, x2, x3], 1)
+    }
 }
